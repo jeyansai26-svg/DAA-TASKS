857bab620dad6facb7f22676d14df4041586221f
jeyansai26-svg <jeyansai26@gmail.com>
2025-08-25 11:27:16 +0530

    Add files via upload

---
+++ Task1/task1.6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+// Increment i by multiplication
+for (int i = 1; i <= n; i *= c) { 
+    // some expressions 
+}
+
+// Decrement i by division
+for (int i = n; i > 0; i /= c) { 
+    // some expressions
+}
+}
